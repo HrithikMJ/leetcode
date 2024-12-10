5e9154594d6cc2d234cabb1098dcd64bcfcd350c
HrithikMJ <hrithikmj003@gmail.com>
2024-12-10 08:57:46 +0530

    Update: problems did until 2024/12/10 08:57:46

--- maxSubArray.py
+++ maxSubArray.py
@@ -1,4 +1,13 @@
 # max sub array
+# my initial solution was to find the subarray till the last value
+# eg [-2,1,-3,4,-1,2,1,-5,4], my sol was 4-4
+# but this isn't right, the correct ans is[4,-1,2,1]
+# the sub array should stop at the maximum value
+# so i used a 2ptr approach
+# maxVal holds the best ever value
+# maxEnd checks all the best values
+
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxVal = nums[0]
